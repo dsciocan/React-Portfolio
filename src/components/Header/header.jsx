063ec6972aab855f3137c714967d8c4fcc0cd604
Daniela <daniela.ciocan137@gmail.com>
2024-03-20 17:14:19 +0000

    Minor style changes

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -8,12 +8,12 @@ function Header() {
     return (
     <>
         <header className="header">
-            <div className="container-fluid px-4 py-2">
+            <div className="container-fluid py-2">
             <div className="row d-flex align-items-center justify-content-between">
-            <div className="col-10 col-sm-8 col-lg-6">
+            <div className="col-10 col-sm-8 col-lg-5">
             <img src="/assets/self-pic.jpg" className="d-block mx-lg-auto rounded-circle " alt="Picture of myself" loading="lazy"/>
             </div>
-            <div className="col-lg-6">
+            <div className="col-lg-7" id="text">
                 <h1 className="display-5 fw-bold lh-1 mb-3">Passionate Front End Web Developer</h1>
                 <p className="lead">Hey there! I'm Daniela, a dedicated web developer on a constant quest for improvement. Explore my portfolio to see what I'm passionate about.</p>
                 <p className="lead">Questions, comments, or suggestions? I'd love to hear from you! Drop me a line via the <Link id="head-link" to="/contact">contact</Link> section.</p>
